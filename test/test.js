c81c02654fcb36063f9fca4bac0a2307b34a32ae
dengnanhao <dengnanhao@hddata.cn>
2023-05-09 13:54:40 +0800

    feat(test): 集成单元测试库mocha、expect

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const expect = require("expect.js")
+
+describe("单元测试", function () {
+    describe("hello", function (){
+        it('hello', function () {
+            expect(1).to.equal(1)
+        })
+    })
+})
